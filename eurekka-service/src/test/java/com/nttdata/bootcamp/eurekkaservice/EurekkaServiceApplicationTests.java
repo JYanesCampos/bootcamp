d86f60451120bd20e979bff77db2622ff7598f3e
Joaquin Yanes Campos <93703559+JYanesCampos@users.noreply.github.com>
2022-10-24 13:13:50 +0200

    Add files via upload

---
+++ eurekka-service/src/test/java/com/nttdata/bootcamp/eurekkaservice/EurekkaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.eurekkaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekkaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
